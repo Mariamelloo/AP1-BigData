7fbb588546b2279737ba8bda7886139fbae07aca
Mariamelloo <105370969+Mariamelloo@users.noreply.github.com>
2024-09-28 21:54:19 -0300

    Add files via upload

---
+++ ap1/src/main/java/ap1/bigdata/repository/ClienteRepository.java
@@ -0,0 +1,16 @@
+package ap1.bigdata.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ap1.bigdata.ap1.model.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Cliente> findByEmail(String email);
+    Optional<Cliente> findByCpf(String cpf);
+}
+
+
