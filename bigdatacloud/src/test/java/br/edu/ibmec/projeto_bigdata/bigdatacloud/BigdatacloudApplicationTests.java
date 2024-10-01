f791fc511e1852b95454beb9544c97462a387d47
Maria Mello <maria.mello2910@gmail.com>
2024-10-01 18:40:16 -0300

    Bugfix: tirando erro

--- bigdatacloud/src/test/java/br/edu/ibmec/projeto_bigdata/bigdatacloud/BigdatacloudApplicationTests.java
+++ bigdatacloud/src/test/java/br/edu/ibmec/projeto_bigdata/bigdatacloud/BigdatacloudApplicationTests.java
@@ -1,13 +1,8 @@
 package br.edu.ibmec.projeto_bigdata.bigdatacloud;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BigdatacloudApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
